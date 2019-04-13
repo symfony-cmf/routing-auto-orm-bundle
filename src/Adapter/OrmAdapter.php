fc0b245c9d092a75106444d228efe846ab74b724
Alfonso Machado <amachado@wearemarketing.com>
2019-04-13 12:35:27 +0200

    remove route of content when remove the auto route

--- src/Adapter/OrmAdapter.php
+++ src/Adapter/OrmAdapter.php
@@ -114,6 +114,11 @@ class OrmAdapter implements AdapterInterface
      */
     public function removeAutoRoute(AutoRouteInterface $autoRoute)
     {
+        $content = $autoRoute->getContent();
+        if ($content) {
+            $content->removeRoute($autoRoute);
+        }
+
         $this->em->remove($autoRoute);
         $this->em->flush($autoRoute);
     }
